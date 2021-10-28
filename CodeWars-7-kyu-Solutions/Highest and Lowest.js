7f02a14a0088526d27e9369e2929d43329dc9851
art2url <artem.turlenko@gmail.com>
2021-10-28 12:07:23 +0300

    upd

---
+++ CodeWars-7-kyu-Solutions/Highest and Lowest.js
@@ -0,0 +1,14 @@
+// In this little assignment you are given a string 
+// of space separated numbers, and have to return 
+// the highest and lowest number.
+
+// Notes
+// All numbers are valid Int32, no need to validate them.
+// There will always be at least one number in the input string.
+// Output string must be two numbers separated by a single space,
+// and highest number is first.
+
+function highAndLow(numbers) {
+    let sorted = numbers.split(' ').sort((a, b) => a - b);
+    return sorted[sorted.length - 1] + ' ' + sorted[0];
+}
