6ec32306cbfbcf82200651d63e69f48d1a2d2225
Artem Turlenko <76534386+art2url@users.noreply.github.com>
2021-11-10 12:34:31 +0200

    Create Sort by Last Char.js

---
+++ CodeWars-7-kyu-Solutions/Sort by Last Char.js
@@ -0,0 +1,15 @@
+// Given a string of words (x), you need to return 
+// an array of the words, sorted alphabetically by 
+// the final character in each.
+
+// If two words have the same last letter, they 
+// returned array should show them in the order 
+// they appeared in the given string.
+
+// All inputs will be valid.
+
+function last(arr) {
+    let strArr = arr.split(' ');
+    return strArr.sort((a, b) => a.charCodeAt(a.length - 1) - b.charCodeAt(b.length - 1));
+}
+last('take me to semynak');
