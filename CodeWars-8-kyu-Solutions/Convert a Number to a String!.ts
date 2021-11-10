7ff4911b0e31e9139a2bc83a02ce8d2c83d89158
Artem Turlenko <76534386+art2url@users.noreply.github.com>
2021-11-10 15:01:07 +0200

    Create Convert a Number to a String!.ts

---
+++ CodeWars-8-kyu-Solutions/Convert a Number to a String!.ts
@@ -0,0 +1,10 @@
+// We need a function that can transform a number into a string.
+// What ways of achieving this do you know?
+
+// Examples:
+// 123 --> "123"
+// 999 --> "999"
+
+export function numberToString(num: number): string {
+    return num.toString();
+  }
