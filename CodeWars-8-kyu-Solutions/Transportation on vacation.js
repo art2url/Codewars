047ca15b689a3e04bb23d2b367458254fef764cb
art2url <artem.turlenko@gmail.com>
2021-11-15 20:05:31 +0200

    Create Transportation on vacation.js

---
+++ CodeWars-8-kyu-Solutions/Transportation on vacation.js
@@ -0,0 +1,33 @@
+// After a hard quarter in the office you decide 
+// to get some rest on a vacation. So you will 
+// book a flight for you and your girlfriend 
+// and try to leave all the mess behind you.
+
+// You will need a rental car in order for you 
+// to get around in your vacation. The manager 
+// of the car rental makes you some good offers.
+
+// Every day you rent the car costs $40. If you 
+// rent the car for 7 or more days, you get $50 
+// off your total. Alternatively, if you rent 
+// the car for 3 or more days, 
+// you get $20 off your total.
+
+// Write a code that gives out the total amount 
+// for different days(d).
+
+function rentalCarCost(d) {
+    let sum = 0;
+
+    for (let i = 1; i <= d; i++) {
+        sum += 40;
+    }
+
+    if (d < 3) {
+        return sum;
+    } else if (d <= 6) {
+        return sum -= 20;
+    } else if (d < 365) {
+        return sum -= 50;
+    }
+}
