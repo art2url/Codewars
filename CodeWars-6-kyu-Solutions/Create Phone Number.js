f0a2ba0b14559c8fe5428cf12fe58f0b1c6de675
art2url <artem.turlenko@gmail.com>
2021-11-17 22:19:58 +0200

    Update Create Phone Number.js

--- CodeWars-6-kyu-Solutions/Create Phone Number.js
+++ CodeWars-6-kyu-Solutions/Create Phone Number.js
@@ -6,6 +6,17 @@
 // to complete this challenge.
 // Don't forget the space after the closing parentheses!
 
+//v1
+function createPhoneNumber(numbers) {
+    let sprite = '(xxx) xxx-xxxx';
+    numbers.forEach(item => {
+        sprite = sprite.replace('x', item);
+    });
+    return sprite;
+}
+createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
+
+// v2
 function createPhoneNumber(numbers) {
     return (`(${numbers[0]}${numbers[1]}${numbers[2]}) ${numbers[3]}${numbers[4]}${numbers[5]}-${numbers[6]}${numbers[7]}${numbers[8]}${numbers[9]}`)
 }
