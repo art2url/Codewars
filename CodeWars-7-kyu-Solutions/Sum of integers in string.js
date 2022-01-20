be692024a03b0147c58e4dcf4c58b909d9f977f0
art2url <artem.turlenko@gmail.com>
2022-01-20 20:53:09 +0200

    Create Sum of integers in string.js

---
+++ CodeWars-7-kyu-Solutions/Sum of integers in string.js
@@ -0,0 +1,18 @@
+// Your task in this kata is to implement a function that calculates 
+// the sum of the integers inside a string. For example, in the string 
+// "The30quick20brown10f0x1203jumps914ov3r1349the102l4zy dog", 
+// the sum of the integers is 3635.
+
+// Note: only positive integers will be tested.
+
+const sumOfIntegersInString = (s) => {
+    const numbers = s.match(/\d+/g).map(Number);
+    const count = numbers.reduce(function(sum, current) {
+        return sum + current;
+    }, 0);
+    return count;
+}
+
+sumOfIntegersInString(
+    "The30quick20brown10f0x1203jumps914ov3r1349the102l4zy dog"
+)
