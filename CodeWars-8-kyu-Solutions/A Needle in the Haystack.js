469914cd7c8f6fe862a2fcdcbfd8d8437b582975
Artem Turlenko <76534386+art2url@users.noreply.github.com>
2024-11-23 15:43:16 +0100

    8-kyu – A Needle in the Haystack.js

---
+++ CodeWars-8-kyu-Solutions/A Needle in the Haystack.js
@@ -0,0 +1,32 @@
+/*
+Can you find the needle in the haystack?
+
+Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+After your function finds the needle it should return a message (as a string) that says:
+
+"found the needle at position " plus the index it found the needle, so:
+
+Example(Input --> Output)
+
+["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"] --> "found the needle at position 5" 
+Note: In COBOL, it should return "found the needle at position 6"
+*/
+
+function findNeedle(haystack) {
+  const position = haystack.indexOf('needle');
+  return `found the needle at position ${position}`;
+}
+
+findNeedle([
+  '3',
+  '123124234',
+  undefined,
+  'needle',
+  'world',
+  'hay',
+  2,
+  '3',
+  true,
+  false,
+]);
