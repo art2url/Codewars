fc76533e21ad96b32111d263caca2b1c24d641c3
Artem Turlenko <artem.turlenko@gmail.com>
2024-12-27 10:20:39 +0100

    8-kyu – Reversed sequence.ts

---
+++ CodeWars-8-kyu-Solutions/Reversed sequence.ts
@@ -0,0 +1,11 @@
+/*
+Build a function that returns an array of integers from n to 1 where n>0.
+
+Example : n=5 --> [5,4,3,2,1]
+*/
+
+export const reverseSeq = (n: number): number[] => {
+  return Array.from({ length: n }, (_, i) => n - i);
+};
+
+reverseSeq(5); // [5, 4, 3, 2, 1]
